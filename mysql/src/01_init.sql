3b6b504c6b55b6b0136728e5a5ab1d28c8bb1719
Ryan McDonagh <ryanjmcdonagh@gmail.com>
2019-02-27 16:23:47 +0000

    Initial MySQL Setup #1

---
+++ mysql/src/01_init.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE remindyou;
+
+USE remindyou;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fName VARCHAR(30) NOT NULL,
+    lName VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    pass VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE lists (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user INT NOT NULL,
+
+    FOREIGN KEY (user) REFERENCES users(id)
+);
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    list INT NOT NULL,
+    article text NOT NULL,
+
+    FOREIGN KEY (list) REFERENCES lists(id)
+);
